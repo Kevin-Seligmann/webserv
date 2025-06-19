e91415ca694d21248d53b24f47a5c2c898e98ab9
martin <martin.visca@gmail.com>
2025-06-19 17:00:01 +0200

    delete objs

--- obj/Servers.d
+++
@@ -1,6 +0,0 @@
-obj/Servers.o: src/Servers.cpp src/../inc/Servers.hpp \
- src/../inc/Parsed.hpp src/../inc/Listen.hpp src/../inc/Locations.hpp
-src/../inc/Servers.hpp:
-src/../inc/Parsed.hpp:
-src/../inc/Listen.hpp:
-src/../inc/Locations.hpp:
