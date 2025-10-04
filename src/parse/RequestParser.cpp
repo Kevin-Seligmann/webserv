01d2f5967fa7a96256e651b9b02e5fe0e039b888
Kevin Seligmann <seligmann.kevin.cc@gmail.com>
2025-10-04 11:36:16 +0200

    fix

--- src/parse/RequestParser.cpp
+++ src/parse/RequestParser.cpp
@@ -738,8 +738,8 @@ std::string const  RequestParser::get_remainder() const
 
 ssize_t RequestParser::extract_buffer_chunk(uint8_t * dst, ssize_t buffer_size)
 {    
-    ssize_t extracted = std::min(_buffer.size(), buffer_size);
-    if (extracted != _buffer.size())
+    ssize_t extracted = _chunk_length;
+    if (extracted > buffer_size)
         CODE_ERR("OK");
     memcpy(dst, _buffer.itbegin(), extracted);
     _buffer.unsafe_consume_bytes(extracted);
